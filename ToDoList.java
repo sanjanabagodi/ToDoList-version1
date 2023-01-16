e0122e8ae738c69546dc15ce99bda7e7d661479b
Sanjana Bagodi <71595130+sanjanabagodi@users.noreply.github.com>
2023-01-16 22:08:23 +0530

    Add files via upload

--- ToDoList.java
+++ ToDoList.java
@@ -1,7 +1,29 @@
+//holds main method/function
+
+/* Things to do:
+ * 
+ * 1) Add Classes
+ * 2) Add constructors for all classes
+ * 3) Setup GUI(done while adding constructors)
+ * 4) Functionality:
+ * 		i. click to add new task
+ *		ii. limit max. number of tasks
+ *		iii. update index as you add tasks
+ *		iv. change color or strike-through when clicked done
+ *		v. clear finished tasks
+ * 5)Add color
+ * 
+ */
+
 package classes;
 
 public class ToDoList {
 	public static void main(String[] args) {
-		new AppFrame();		
+		//all its gonna do is make us a new AppFrame
+		new AppFrame();		//pay attention, cause case-sensitive!!
+		//no need to give a name to the new AppFrame, cause we wont be using/referencing it again. only creating
 	}
 }
+
+
+
